23efcc90a61bf07f495b0684d20a881418f2723d
PerttuValtonen <135586722+PerttuValtonen@users.noreply.github.com>
2023-06-14 13:54:37 +0300

    Initial Commit

---
+++ Calories/CaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesApp.swift
+//  Calories
+//
+//  Created by Perttu Valtonen on 14.6.2023.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
